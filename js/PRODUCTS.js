bcce727cad5678b2f230947714b9c3b959efbbb9
cliffordtzlin11 <124945079+cliffordtzlin11@users.noreply.github.com>
2023-05-15 08:18:01 -0700

    Add files via upload

---
+++ js/PRODUCTS.js
@@ -0,0 +1,69 @@
+/*********************************************************************
+ *                          OCEAN CASE                               *
+ * CIS 5620: Authoring Websites                                      *
+ *********************************************************************/
+
+/* Data structure containing all product data displayed on the webpage
+   The data structure in this script is just for referene since products
+   in this version of the site are stored in the cloud.
+*/
+const PRODUCTS = {
+    product_1: {
+        id: "product_1",
+        name: "Recycle Reuse Reduce",
+        description: "iPhone 14 Pro Max - Re/CASETiFY",
+        price: 78.00,
+        stars: 4,
+        image: "assets/img/product/p1.jpg"
+    }, product_2: {
+        id: "product_2",
+        name: "Stars Black",
+        description: "iPhone 14 Pro Max - Re/CASETiFY",
+        price: 92.00,
+        stars: 5,
+        image: "assets/img/product/p2.jpg"
+    }, product_3: {
+        id: "product_3",
+        name: "Cheetah Paradise Pink",
+        description: "Samsung S22 Ultra - Re/CASETiFY",
+        price: 82.00,
+        stars: 4,
+        image: "assets/img/product/p3.jpg"
+    }, product_4: {
+        id: "product_4",
+        name: "Black Boreal",
+        description: "iPhone 14 Pro Max - pela",
+        price: 69.95,
+        stars: 4,
+        image: "assets/img/product/p4.jpg"
+    }, product_5: {
+        id: "product_5",
+        name: "Black Retro Moon",
+        description: "Google Pixel 7 Pro - pela",
+        price: 59.95,
+        stars: 4,
+        image: "assets/img/product/p5.jpg"
+    }, product_6: {
+        id: "product_6",
+        name: "ECO 94 Recycled",
+        description: "iPhone 11 Pro Max - CASE MATE",
+        price: 25.00,
+        stars: 2,
+        image: "assets/img/product/p6.jpg"
+    }, product_7: {
+        id: "product_7",
+        name: "Green Summit",
+        description: "Samsung S23 - pela",
+        price: 59.95,
+        stars: 5,
+        image: "assets/img/product/p7.jpg"
+    }, product_8: {
+        id: "product_8",
+        name: "Aqua Smiley",
+        description: "Google Pixel 6 Pro - pela",
+        price: 48.00,
+        stars: 3,
+        image: "assets/img/product/p8.jpg"
+    }
+    
+};
